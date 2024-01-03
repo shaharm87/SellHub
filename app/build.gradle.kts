e88b39b2a0f94bc1d74412f9a1d50fb8eb1de0d6
Shahar <Shahar@gmail.com>
2024-01-03 22:38:30 +0200

    init firebase plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+	id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,8 @@ android {
 
 dependencies {
 
+	implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+	implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")

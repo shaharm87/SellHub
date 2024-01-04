c72267be94ec6aa231d55ae634a1ac47ed09202c
Shahar <shaharm87@gmail.com>
2024-01-04 23:37:15 +0200

    added google services file

--- app/src/main/java/com/example/sellhub/Register.kt
+++ app/src/main/java/com/example/sellhub/Register.kt
@@ -19,7 +19,7 @@ class Register : AppCompatActivity() {
         setContentView(R.layout.activity_register)
     }
 
-    fun onButtonClick(view: View) {
+    fun onGoClick(view: View) {
         try {
             val email = findViewById<TextInputEditText>(R.id.reg_email).text.toString()
             val password = findViewById<TextInputEditText>(R.id.reg_password).text.toString()
@@ -38,4 +38,23 @@ class Register : AppCompatActivity() {
             Log.d("Tag", e.message.toString());
         }
     }
+
+    fun onLoginClick(view: View) {
+        try {
+            val email = findViewById<TextInputEditText>(R.id.reg_email).text.toString()
+            val password = findViewById<TextInputEditText>(R.id.reg_password).text.toString()
+            userManager.loginUser(email, password) { isSuccess, errorMessage ->
+                if (isSuccess) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Login failed: $errorMessage", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } catch (e: Exception) {
+            Log.d("Tag", e.message.toString());
+        }
+    }
+
+
 }

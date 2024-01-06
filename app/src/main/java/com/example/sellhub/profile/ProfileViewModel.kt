f82a260d5f1dd6403132ab383a0772fa38f081cf
Dor Zilberman <dorzil1998@gmail.com>
2024-01-06 21:24:46 +0200

    show only posts by user in profile page

--- app/src/main/java/com/example/sellhub/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/sellhub/profile/ProfileViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.sellhub.profile
 
 import androidx.lifecycle.ViewModel
+import com.example.sellhub.managers.UserManager
 import com.example.sellhub.newitem.Item
 import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileViewModel : ViewModel() {
     private val db = FirebaseFirestore.getInstance()
     private val collectionRef = db.collection("items")
+    private val userManager = UserManager()
 
     fun getItems(callback: (Boolean, List<Item>) -> Unit) {
         collectionRef.get()
@@ -16,11 +18,13 @@ class ProfileViewModel : ViewModel() {
                     val data = document.data
                     val id = document.id
                     val displayName = data["displayName"] as String
-                    val title = data["title"] as String
-                    val description = data["description"] as String
-
-                    val item = Item(displayName, title, description)
-                    items.add(item)
+                    val currentUser = userManager.getCurrentUser()
+                    if (displayName == currentUser?.displayName) {
+                        val title = data["title"] as String
+                        val description = data["description"] as String
+                        val item = Item(displayName, title, description)
+                        items.add(item)
+                    }
                 }
                 callback(true, items)
             }

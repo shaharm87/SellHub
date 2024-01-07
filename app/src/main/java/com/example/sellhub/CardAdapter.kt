73a6b899d8891865ff7d66cedd670f027b3a686e
Shahar <shaharm87@gmail.com>
2024-01-07 20:57:33 +0200

    added chips for item type

--- app/src/main/java/com/example/sellhub/CardAdapter.kt
+++ app/src/main/java/com/example/sellhub/CardAdapter.kt
@@ -8,6 +8,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sellhub.newitem.Item
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.sql.Struct
 
 data class CardData(
     var item: Item,
@@ -29,8 +32,19 @@ class CardAdapter(private val cardList: List<CardData>) :
         holder.titleTextView.text = currentItem.item.title
         holder.descriptionTextView.text = currentItem.item.description
         holder.displayNameTextView.text = currentItem.item.displayName
+
+        holder.chipGroup.removeAllViews()
+        currentItem.item.types?.forEach { type ->
+            val chip = Chip(holder.chipGroup.context)
+            chip.text = type
+            chip.isClickable = false
+            chip.isCheckable = false
+            setChipIcon(type, chip)
+            holder.chipGroup.addView(chip)
+        }
         if (currentItem.image != null)
             holder.displayImage.setImageBitmap(currentItem.image)
+
 
     }
 
@@ -44,6 +58,21 @@ class CardAdapter(private val cardList: List<CardData>) :
         val descriptionTextView: TextView = itemView.findViewById(R.id.card_description)
         val displayNameTextView: TextView = itemView.findViewById(R.id.card_user_name)
         val displayImage: ImageView = itemView.findViewById(R.id.card_image)
+        val chipGroup: ChipGroup = itemView.findViewById(R.id.card_chip_group)
 
+
+    }
+
+    fun setChipIcon(type: String, chip: Chip) {
+        if (type == "Electronics")
+            chip.setChipIconResource(R.drawable.electronic)
+        if (type == "Games")
+            chip.setChipIconResource(R.drawable.games)
+        if (type == "Clothing")
+            chip.setChipIconResource(R.drawable.clothing)
+        if (type == "Jewellery")
+            chip.setChipIconResource(R.drawable.diamond)
+        if (type == "Furniture")
+            chip.setChipIconResource(R.drawable.bed)
     }
 }
